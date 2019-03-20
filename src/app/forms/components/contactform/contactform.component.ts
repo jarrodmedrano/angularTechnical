16f0d5072e891d5fd95ad52647f6b77976b366ed
Jarrod Medrano <jmedran@gmail.com>
2019-03-19 20:59:04 -0600

    adding basic validation

--- src/app/forms/components/contactform/contactform.component.ts
+++ src/app/forms/components/contactform/contactform.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contactform',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contactform.component.css']
 })
 export class ContactFormComponent implements OnInit {
+  contactForm: FormGroup;
 
   constructor() { }
 
   ngOnInit() {
+    this.contactForm = new FormGroup( {
+      'name': new FormControl(null, Validators.required),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'message': new FormControl(null),
+      'shouldAddToNewsletter': new FormControl(false)
+    });
   }
 
 }
