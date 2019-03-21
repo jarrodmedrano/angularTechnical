5e220f3800117977146542cd47da84be9ec7cb75
Jarrod Medrano <jmedran@gmail.com>
2019-03-20 20:50:17 -0600

    convert submission to html and log it

--- src/app/services/send-mail.service.ts
+++ src/app/services/send-mail.service.ts
@@ -6,8 +6,17 @@ export class SendMailService {
 
   constructor() {}
 
-  logFormSubmission(submitted: {}) {
-    console.log(submitted);
+  logFormSubmission(submitted: {name: string, email: string, message: string, shouldAddToNewsletter: boolean}) {
+    const {name, email, message, shouldAddToNewsletter} = submitted;
+    const date = Date();
+    const html = `
+      <div>From: ${name}</div>
+      <div>Email: <a href="mailto:${email}">${email}</a></div>
+      <div>Date: ${date}</div>
+      <div>Message: ${message}</div>
+      <div>Should Add to Newsletter: ${shouldAddToNewsletter}</div>
+    `;
+    console.log(html);
   }
 
 }
