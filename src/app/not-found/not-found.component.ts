da2196526dbc53d2213d4581dedfa4950e1170c2
Jarrod Medrano <jmedran@gmail.com>
2019-03-21 22:14:30 -0600

    adding goofy 404 page

--- src/app/not-found/not-found.component.ts
+++ src/app/not-found/not-found.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
   styleUrls: ['./not-found.component.css']
 })
-export class NotFoundComponent { }
+export class NotFoundComponent implements OnInit, OnDestroy {
+
+  ngOnInit(): void {
+    const body = document.getElementsByTagName('body')[0];
+    body.classList.add('notfound');
+  }
+
+  ngOnDestroy(): void {
+    const body = document.getElementsByTagName('body')[0];
+    body.classList.remove('notfound');
+  }
+
+}
